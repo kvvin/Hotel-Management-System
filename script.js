f6ad1dc01f784f2aebea75849080fb915a295fb6
Kevin Thomas Manjooran <kevinthomastm2002@gmail.com>
2023-07-02 17:46:46 +0530

    login/signup page

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const navBtn = document.getElementById('nav-btn');
 const cancelBtn = document.getElementById('cancel-btn');
 const sideNav = document.getElementById('sidenav');
 const modal = document.getElementById('modal');
-
 navBtn.addEventListener("click", function(){
     sideNav.classList.add('show');
     modal.classList.add('showModal');
@@ -19,3 +18,18 @@ window.addEventListener('click', function(event){
         modal.classList.remove('showModal');
     }
 });
+  
+function openLoginPage() {
+    window.open('login.html', '_blank');
+  }
+  
+  // Function to open the signup page
+  function openSignupPage() {
+    window.open('signup.html', '_blank');
+  }
+  
+  // Function to handle successful login and redirect back to the main webpage
+  function handleSuccessfulLogin() {
+    // Redirect back to the main webpage
+    window.location.href = 'index.html'; // Update with the appropriate URL of your main webpage
+  }
